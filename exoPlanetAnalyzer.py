b7ad7ddee8a193b1ea6867833517a70ac9bd58cb
jusbro <97056196+jusbro@users.noreply.github.com>
2023-11-16 14:47:51 -0500

    Update exoPlanetAnalyzer.py

--- exoPlanetAnalyzer.py
+++ exoPlanetAnalyzer.py
@@ -5,10 +5,14 @@ def read_star_data():
     with open("star_data.csv", 'r') as file:
         csvreader = csv.reader(file)
         header = next(csvreader)
+        min_Luminosity = file['luminosity'].min()
+        print(min_Luminosity)
         for row in csvreader:
             rows.append(row)
     print(header)
     print(rows)
+
+
 
 if __name__ == "__main__":
     read_star_data()
